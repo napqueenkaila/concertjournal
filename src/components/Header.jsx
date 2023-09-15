b89cdb16fed78a0fc373c65eaa67c49bffc88d51
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-09-15 14:31:42 -0400

    install tailwind css & update styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
-export default function Header(){
+import { Link } from "react-router-dom"
+
+export default function Header() {
     return (
-        <header>
-            <i className="fa-solid fa-music"></i>
-            <p className="header-text">kaila&apos;s concert journal.</p>
-        </header>
+        <Link to="/">
+            <header className="bg-accent text-white text-xl font-medium tracking-tighter p-8 sm:p-12 md:p-14 lg:p-16 sm:text-2xl md:text-3xl lg:text-4xl flex justify-center items-center gap-2" >
+                <i className="fa-solid fa-music"></i>
+                <p>kaila&apos;s concert journal.</p>
+            </header>
+        </Link>
     )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,jsx}",
+    "./pages/**/*.{html, jsx}",
+    "./components/**/*.{html, jsx}"
+  ],
+  theme: {
+    colors: {
+      accent: '#F55A5A',
+      white: '#ffffff',
+      black: '#2B283A'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
